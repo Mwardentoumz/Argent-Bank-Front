08b3ecc4f57ed1f6e5123168abcd4729e33f730c
Mwardentoumz <102516841+Mwardentoumz@users.noreply.github.com>
2023-04-13 11:20:09 +0200

    Delete store.jsx

--- argentbank/src/utils/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../utils/reducers";
-
-export const store = configureStore({
-    reducer: {
-        user: userReducer,
-    },
-});
