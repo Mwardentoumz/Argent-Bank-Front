6a46b2f816152d9731c570ae1d21fe9c632e2f0e
Mwardentoumz <102516841+Mwardentoumz@users.noreply.github.com>
2023-04-13 11:20:02 +0200

    Delete selectors.jsx

--- argentbank/src/utils/selectors.jsx
+++
@@ -1,9 +0,0 @@
-export const selectUserLogin = (state) => state.user.isLoggedIn;
-
-export const selectUserToken = (state) => state.user.JWTtoken;
-
-export const selectUserData = (state) => state.user.userData;
-
-export const selectFirstName = (state) => state.user.userData.firstName;
-
-export const selectLastName = (state) => state.user.userData.lastName;
